d23620d0d627f1b332cc8c534f56a872ec401d83
Musili-Adebayo <adebayomusili@gmail.com>
2026-09-11 13:06:36 +0100

    new_snowflake_infra

--- snowflake/production/backend.tf
+++ snowflake/production/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "federated-engineers-production-elite-urban-route-tfstate"
+    bucket = "federated-engineers-production-elite-urban-route-tfstate-v2"
     key    = "urban-route/production/terraform.tfstate"
     region = "eu-central-1"
   }
